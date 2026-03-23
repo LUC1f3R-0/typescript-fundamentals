cc7e2cacbbacd3009285702579aa3189ba79d6eb
LUC1f3R-0 <thusharathilan123@gmail.com>
2026-03-23 13:58:50 +0530

    added the send

--- 2.ts
+++ 2.ts
@@ -1,1 +1,18 @@
-console.log("entered");
+const function1 = (str: Array<string>):number => {
+  
+  const firstletter = str[0];
+  let num: number = 0; 
+  
+  if (str.length != 0) {
+    
+    for (let i = 1; i < str.length; i++){
+      if (firstletter == str[i])
+        num++;
+    }
+  }
+  return num;
+};
+
+const stringOne: Array<string> = ["egrbihj", "egrbihj", "wecrfhinuql", "egrbihj"];
+
+console.log(function1(stringOne));
