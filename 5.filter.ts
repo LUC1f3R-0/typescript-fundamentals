d118d1d4f1ee406b0126df5b8aa0c6e9dd864be7
LUC1f3R-0 <thusharathilan123@gmail.com>
2026-03-23 18:10:01 +0530

    added another

---
+++ 5.filter.ts
@@ -0,0 +1,14 @@
+const fun = (pre: string, str: Array<string>): Array<string>| string => { 
+  
+  
+  return "";
+}
+
+
+const fun1 = (num: number, numArr: Array<number>): Array<number> | number => {
+  
+  const newArr = numArr.filter(n => 5 > n);
+  return newArr;;
+}
+
+console.log(fun1(5, []));
